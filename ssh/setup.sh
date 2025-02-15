f0ba5ec15647bf8f8f07274fa43be3d52dda852a
Pere Bohigas <pere.bohigas@gmail.com>
2025-02-15 19:49:41 +0100

    Move SSH hosts configuration into a separate directory

--- ssh/setup.sh
+++ ssh/setup.sh
@@ -2,5 +2,6 @@
 
 [ ! -d "$HOME/.ssh" ] && mkdir -p "$HOME/.ssh"
 ln -s "$PWD/config" "$HOME/.ssh/config"
+ln -s "$PWD/hosts" "$HOME/.ssh/hosts"
 chmod 600 config # Give read and write permissions to the current user only (strict permissions) due to abuse potential
 
